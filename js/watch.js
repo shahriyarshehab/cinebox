a6743d7a04d3a473ee240c6e68c194bc1b9f3ed7
Shahriyar Shehab <shahriyarshehab@users.noreply.github.com>
2026-08-26 13:40:48 +0600

    Change episode item 1st button to dedicated Stream button and remove season batch download button

--- js/watch.js
+++ js/watch.js
@@ -1438,8 +1438,8 @@ function renderEpisodeListHtml(episodes) {
                 </div>
             </div>
             <div class="ep-action-btns" onclick="event.stopPropagation();">
-                <button class="ep-icon-btn" onclick="openExternalPlayersModal('${ep.url}', '${escapeQuotes(ep.name)}')" title="External Players (VLC, MX, PotPlayer)">
-                    <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="13" height="13"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
+                <button class="ep-icon-btn ep-btn-stream" onclick="playSpecificEpisode(${originalIdx})" title="Stream Episode">
+                    <svg class="icon" viewBox="0 0 24 24" fill="currentColor" width="13" height="13"><path d="M8 5v14l11-7z"/></svg>
                 </button>
                 <button class="ep-icon-btn" onclick="openDownloadModal('${ep.url}', '${escapeQuotes(ep.name)}')" title="Download Episode">
                     <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="13" height="13"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
@@ -1480,8 +1480,8 @@ function renderPlayerEpisodeList(episodes) {
                 </div>
             </div>
             <div class="ep-action-btns" onclick="event.stopPropagation();">
-                <button class="ep-icon-btn" onclick="openExternalPlayersModal('${ep.url}', '${escapeQuotes(ep.name)}')" title="External Players">
-                    <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="13" height="13"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
+                <button class="ep-icon-btn ep-btn-stream" onclick="playSpecificEpisode(${originalIdx})" title="Stream Episode">
+                    <svg class="icon" viewBox="0 0 24 24" fill="currentColor" width="13" height="13"><path d="M8 5v14l11-7z"/></svg>
                 </button>
                 <button class="ep-icon-btn" onclick="openDownloadModal('${ep.url}', '${escapeQuotes(ep.name)}')" title="Download Episode">
                     <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="13" height="13"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
